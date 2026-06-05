7e930559ee9937facc9642192f0701e66dfb784e
catini <joaovitorcc.07@gmail.com>
2026-06-05 19:29:50 -0300

    redundancies were eliminated and code is, in general, cleaner

--- deck.py
+++ deck.py
@@ -1,6 +1,11 @@
 import subprocess
 import random
 
+def clear():
+    subprocess.run(
+        ['cls' if os.name == 'nt' else 'clear']
+    )
+
 class card:
     def __init__(self, suit, number):
         self.suit = suit
@@ -12,27 +17,25 @@ class deck:
         self.collection = {}
         suits = ["♠", "♡", "♦", "♣"]
         numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-        counter = 0 # helper variable that allows card attributes to be named
+        self.quantity = len(suits) * len(numbers) # dynamically counts the cards
+        counter = 0 # helper variable to populate the collection set
         for s in suits: # dynamically creates the card collection
             for n in numbers:
-                attr_name = f"card{counter}"
-                newcard = card(s, n)
-                self.collection[attr_name] = newcard
+                self.collection[f"card{counter}"] = card(s, n)
                 counter += 1
-        self.order = ["" for i in range(counter)]
-        for c in range(counter): # inserts all cards in a set, representing the card order, that can later be suffled
-            self.order[c] = self.collection[f"card{c}"].string
+        self.order = [self.collection[f"card{c}"].string for c in range(self.quantity)] # set that actually stores the order of the cards
+
     def shuffle(self):
         random.shuffle(self.order)
     def pick_first(self):
         return self.order[0]
     def pick_any(self):
-        return self.order[random.randint(0, 52)]
+        return self.order[random.randint(0, self.quantity)] # 
     
 
 deck1 = deck()
 
-for i in range(52):
+for i in range(deck1.quantity):
     print(deck1.collection[f"card{i}"].string)
 
 print(f"\n")
