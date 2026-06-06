ddab245cb17b912eb385ae9671aaccf50f62d9e3
catini <joaovitorcc.07@gmail.com>
2026-06-06 17:53:56 -0300

    exploration menu options 1, 2 and 4 are fully functional.

--- deck.py
+++ deck.py
@@ -8,6 +8,9 @@ def clear():
         ['cls' if os.name == 'nt' else 'clear']
     )
 
+suits = ["♠", "♡", "♦", "♣"]
+numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
 class card:
     def __init__(self, suit, number):
         self.suit = suit
@@ -17,8 +20,6 @@ class card:
 class deck:
     def __init__(self):
         self.collection = {}
-        suits = ["♠", "♡", "♦", "♣"]
-        numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
         self.quantity = len(suits) * len(numbers) # dynamically counts the cards
         counter = 0 # helper variable to populate the collection set
         for s in suits: # dynamically creates the card collection
@@ -33,13 +34,50 @@ class deck:
         return self.order[0]
     def pick_any(self):
         return self.order[random.randint(0, self.quantity)] # 
-    
+    def pick_in_position(self, position):
+        return self.order[position]
+
+def shuffle_anim():# shuffle animation
+    for _ in range(3):
+        clear()
+        print("Shuffling")
+        time.sleep(0.2)
+        clear()
+        print("Shuffling.")
+        time.sleep(0.2)
+        clear()
+        print("Shuffling..")
+        time.sleep(0.2)
+        clear()
+        print("Shuffling...")
+        time.sleep(0.2)
+
+    clear()
+    print("Deck shuffled!")
+    time.sleep(1)
+
+def pick_anim(): # pick animation
+    for _ in range(3):
+        clear()
+        print("Picking")
+        time.sleep(0.2)
+        clear()
+        print("Picking.")
+        time.sleep(0.2)
+        clear()
+        print("Picking..")
+        time.sleep(0.2)
+        clear()
+        print("Picking...")
+        time.sleep(0.2)
+    clear()
 
 menu_state = 0
+expl_menu_state = 0
 deck_is_created = False
 
-while True: # main menu
-    if menu_state == 0:
+while True:
+    if menu_state == 0: # main menu
         if deck_is_created == False:
             clear()
             print(
@@ -47,7 +85,7 @@ while True: # main menu
                 "\n" \
                 "-------------------Main menu---------------------" \
                 "\n\n\n" \
-                "[1] - Create deck\n[2] - Play Blackjack\n[3] - End session" \
+                "[1] - Create deck\n\n[2] - Play Blackjack\n\n[3] - End session" \
                 "\n\n"
                 )
             menu_state_first = int(input("Desired option: "))
@@ -55,8 +93,9 @@ while True: # main menu
             if menu_state_first == 1:
                 clear()
                 deck_is_created = True
+                deck1 = deck()
                 print("Deck created!")
-                time.sleep(2)
+                time.sleep(1)
                 continue
             else:
                 menu_state_first = menu_state
@@ -68,8 +107,50 @@ while True: # main menu
                 "\n" \
                 "-------------------Main menu---------------------" \
                 "\n\n\n" \
-                "[1] - Explore deck\n[2] - Play Blackjack\n[3] - End session" \
+                "[1] - Explore deck\n\n[2] - Play Blackjack\n\n[3] - End session" \
                 "\n\n"
                 )
             menu_state = int(input("Desired option: "))
             continue
+    if menu_state == 1:
+        if expl_menu_state == 0:
+            print(
+                    "------------INTERACTIVE DECK OF CARDS------------" \
+                    "\n" \
+                    "-----------------Exploring deck------------------" \
+                    "\n\n\n" \
+                    "[1] - Shuffle deck\n\n[2] - Pick first card\n\n[3] - Pick any card\n\n[4] - Pick specific card\n\n[5] - Exit exploration" \
+                    "\n\n"
+                    )
+            expl_menu_state = int(input("Desired option: "))
+            continue
+        elif expl_menu_state == 1:
+            shuffle_anim()
+            deck1.shuffle()
+            clear()
+            expl_menu_state = 0
+            continue
+        elif expl_menu_state == 2:
+            clear()
+            pick_anim()
+            clear()
+            print(f"The first card is {deck1.pick_first()}!")
+
+            enter = input("\n\n[ENTER] = Return")
+            expl_menu_state = 0
+            continue
+        elif expl_menu_state == 4:
+            clear()
+            print(f"Choose a position from 0 to {deck1.quantity}.\n-> In this case, 0 is the card on top and {deck1.quantity} is the card in the bottom.")
+            pick_position = int(input("\nDesired position: "))
+            picked_in_pos = deck1.pick_in_position(pick_position)
+            pick_anim()
+            clear()
+            print(f"Card in position {pick_position} is {picked_in_pos}!")
+
+            enter = input("\n\n[ENTER] = Return")
+            expl_menu_state = 0
+            continue
+
+
+            
